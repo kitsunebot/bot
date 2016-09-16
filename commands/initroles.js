a828cfba3695acb171d9f9d0bbd6a9e238c372d3
Fuechschen <me@fuechschen.org>
2016-09-17 01:36:15 +0200

    update initroles

--- commands/initroles.js
+++ commands/initroles.js
@@ -1,3 +1,5 @@
+var Promise = require('bluebird');
+
 var eris = require('../lib/client');
 var lang = require('../lib/lang');
 var fcache = require('../lib/cache');
@@ -12,27 +14,40 @@ module.exports = {
             required: 5,
             have: '!5'
         });
-        var perm = msg.guild.members.find((member)=> {
+        var perm = msg.channel.guild.members.find((member)=> {
             return member.user.id === eris.user.id
         }).permission;
         if (!perm.has('manageRoles'))return lang.computeResponse(msg, 'roles.failed');
         if (fcache.getGuild(msg.channel.guild.id).initialized)return lang.computeResponse(msg, 'roles.already');
-        Promise.join(eris.createRole().then(role=> {
-            return eris.editRole(msg.channel.guild.id, role.id, {name: lang.computeLangString(msg.channel.guild.id, 'roles.regular', false)});
-        }), eris.createRole().then(role=> {
-            return eris.editRole(msg.channel.guild.id, role.id, {name: lang.computeLangString(msg.channel.guild.id, 'roles.vip', false)});
-        }), eris.createRole().then(role=> {
-            return eris.editRole(msg.channel.guild.id, role.id, {name: lang.computeLangString(msg.channel.guild.id, 'roles.moderator', false)});
-        }), eris.createRole().then(role=> {
-            return eris.editRole(msg.channel.guild.id, role.id, {name: lang.computeLangString(msg.channel.guild.id, 'roles.manager', false)});
-        }), (regular, vip, moderator, manager)=> {
-            db.models.Guild.update({
-                role_regular: regular.id,
-                role_vip: vip.id,
-                role_moderator: moderator.id,
-                role_manager: manager.id,
-                initialized: true
+        Promise.join(eris.createRole(msg.channel.guild.id).then(role=> {
+            console.log(role);
+            return eris.editRole(msg.channel.guild.id, role.id, {
+                name: lang.computeLangString(msg.channel.guild.id, 'roles.manager', false),
+                permissions: 0
             });
+        }), eris.createRole(msg.channel.guild.id).then(role=> {
+            return eris.editRole(msg.channel.guild.id, role.id, {
+                name: lang.computeLangString(msg.channel.guild.id, 'roles.moderator', false),
+                permissions: 0
+            });
+        }), eris.createRole(msg.channel.guild.id).then(role=> {
+            return eris.editRole(msg.channel.guild.id, role.id, {
+                name: lang.computeLangString(msg.channel.guild.id, 'roles.vip', false),
+                permissions: 0
+            });
+        }), eris.createRole(msg.channel.guild.id).then(role=> {
+            return eris.editRole(msg.channel.guild.id, role.id, {
+                name: lang.computeLangString(msg.channel.guild.id, 'roles.regular', false),
+                permissions: 0
+            });
+        }), (manager, moderator, vip, regular)=> {
+            db.models.Guild.update({
+                regular_role: regular.id,
+                vip_role: vip.id,
+                moderator_role: moderator.id,
+                manager_role: manager.id,
+                initialized: true
+            }, {where: {gid: msg.channel.guild.id}});
             eris.createMessage(msg.channel.id, lang.computeResponse(msg, 'roles.init'))
         })
     },
