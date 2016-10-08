fbd2b81a44eef19dd3b1af9fb594782ac3b403b4
Fuechschen <me@fuechschen.org>
2016-10-09 01:20:09 +0200

    ugh, forgot devlog

--- eventhandlers/messageCreate.js
+++ eventhandlers/messageCreate.js
@@ -8,7 +8,6 @@ module.exports = {
     event: 'messageCreate',
     enabled: true,
     handler: (msg)=> {
-        console.log(process.env.bot_ready);
         if(process.env.bot_ready){
             if (msg.channel.guild ? fcache.getGuild(msg.channel.guild.id).getSetting('logging') : true) {
                 story.info('msg', '[' + (msg.channel.guild === undefined ? 0 : msg.channel.guild.shard.id) + '][' + (msg.channel.guild === undefined ? 'Private Message' : msg.channel.guild.name) + '|' + msg.channel.name + '|' + msg.author.username + '#' + msg.author.discriminator + ']: ' + msg.cleanContent);
