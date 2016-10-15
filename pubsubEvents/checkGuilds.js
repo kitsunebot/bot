e761f0463e0f5a847c64b0e2285f91322d17d011
Fuechschen <me@fuechschen.org>
2016-10-16 00:34:17 +0200

    fixing some missing imports

--- pubsubEvents/checkGuilds.js
+++ pubsubEvents/checkGuilds.js
@@ -1,8 +1,9 @@
 var eris = require('../lib/client');
+var db = require('../lib/db');
 
+/** @namespace eris.guilds */
 module.exports = {
     event: 'checkGuilds',
     enabled: true,
     handler: ()=>eris.guilds.map(guild=>db.models.Guild.update({online: true}, {where: {gid: guild.id}}))
-}
-;
+};
--- pubsubEvents/checkUsers.js
+++ pubsubEvents/checkUsers.js
@@ -1,8 +1,9 @@
 var eris = require('../lib/client');
+var db = require('../lib/db');
 
+/** @namespace eris.users */
 module.exports = {
     event: 'checkUsers',
     enabled: true,
     handler: ()=>eris.users.map(user=>db.models.User.update({online: true}, {where: {uid: user.id}}))
-}
-;
+};
