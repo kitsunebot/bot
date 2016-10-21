68152e9439a146e0d1f8035941ebc7418cf9c7a8
Fuechschen <me@fuechschen.org>
2016-10-21 12:02:59 +0200

    fix setting global nick

--- commands/nick_global.js
+++ commands/nick_global.js
@@ -1,6 +1,6 @@
 var eris = require('../lib/client');
 var lang = require('../lib/lang');
-var cache = require('../lib/cache');
+var fcache = require('../lib/cache');
 var pubsub = require('../lib/db');
 
 module.exports = {
@@ -8,23 +8,21 @@ module.exports = {
     enabled: true,
     isSubcommand: true,
     generator: (msg, args)=> {
-        cache.getGlobalUserPerm(msg.author.id).then((perm)=> {
-            if (perm > 6) {
-                var nick = (args.join(' ').trim() !== 'reset' ? args.join(' ').trim() : eris.user.username);
-                eris.guilds.map((g)=> {
-                    return g
-                }).forEach((g, index)=> {
-                    setTimeout(()=> {
-                        eris.editNickname(g.id, nick)
-                    }, index * 10000);
-                });
-                msg.channel.createMessage(lang.computeResponse(msg, 'nick.global'));
-                pubsub.sendEvent('nicknameChange',nick);
-            } else msg.channel.createMessage(lang.computeResponse(msg, 'no_permission', {
-                required: 7,
-                have: perm || 0
-            }));
-        });
+        if (fcache.getGlobalUserPerm(msg.author.id) > 6) {
+            var nick = (args.join(' ').trim() !== 'reset' ? args.join(' ').trim() : eris.user.username);
+            eris.guilds.map((g)=> {
+                return g
+            }).forEach((g, index)=> {
+                setTimeout(()=> {
+                    eris.editNickname(g.id, nick)
+                }, index * 10000);
+            });
+            msg.channel.createMessage(lang.computeResponse(msg, 'nick.global'));
+            pubsub.sendEvent('nicknameChange', nick);
+        } else msg.channel.createMessage(lang.computeResponse(msg, 'no_permission', {
+            required: 7,
+            have: perm || 0
+        }));
         return false;
     },
     options: {
