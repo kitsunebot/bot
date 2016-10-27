4e878d16c47518676ffa9000c6e2f541e731b7fc
Fuechschen <me@fuechschen.org>
2016-10-27 10:16:43 +0200

    push it

---
+++ commands/stats_shard.js
@@ -0,0 +1,25 @@
+var eris = require('../lib/client');
+var lang = require('../lib/lang');
+
+module.exports = {
+    label: 'shards',
+    enabled: true,
+    isSubcommand: true,
+    generator: (msg, args)=> {
+        return lang.computeResponse(msg, 'stats.shards', {
+            status: eris.shards.map(shard=> {
+                return lang.computeResponse(msg, 'stats.shard', {
+                    id: shard.id.toString(),
+                    status: shard.status,
+                    guildCount: shard.guildCount
+                }, true)
+            }).join('\n')
+        });
+    },
+    options: {
+        deleteCommand: true,
+        caseInsensitive: true,
+        aliases: ['shard'],
+        serverOnly: true
+    }
+};
