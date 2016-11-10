ecda2c660aa5681ee6f89d7bcfcdf30000d40a1a
Fuechschen <me@fuechschen.org>
2016-11-10 15:28:35 +0100

    fix it and fuck my brain

--- commands/catgirl_add.js
+++ commands/catgirl_add.js
@@ -9,7 +9,7 @@ module.exports = {
             //todo send args err
             console.log(1);
         } else {
-            db.models.find({where: {id: 'SkyVBlzWg'}}).then((waifu)=> {
+            db.models.Character.find({where: {id: 'SkyVBlzWg'}}).then((waifu)=> {
                 return utils.uploadFile(args[args.length - 1]).then((url)=> {
                     return cch.getGlobalUserPerm(msg.author.id).then((perm)=> {
                         return waifu.createCharacterPicture({link: url, verified: (perm > 5)}).then(()=> {
