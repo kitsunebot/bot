c58c4adbc9a75aea27aa1715d7f0142aa512b3bd
Fuechschen <me@fuechschen.org>
2016-11-10 14:26:46 +0100

    add catgirl command

---
+++ commands/catgirl.js
@@ -0,0 +1,19 @@
+var db = require('../lib/db');
+
+module.exports = {
+    label: 'catgirl',
+    enabled: true,
+    isSubcommand: false,
+    generator: (msg)=> {
+        db.models.CharacterPicture.find({
+            order: 'RAND()', include: [
+                {model: db.models.Character, where: {name: 'Catgirl'}}
+            ]
+        }).then(pic=>msg.channel.createMessage(pic.link));
+    },
+    options: {
+        deleteCommand: false,
+        caseInsensitive: true,
+        alias: ['neko']
+    }
+};
